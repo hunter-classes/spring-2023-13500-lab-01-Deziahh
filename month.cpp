cc5e2f5a7b3230ed3d2c7c3c466092b2fda5690d
Dezzz <dezzz@Dezzzs-MacBook-Air.local>
2023-01-28 14:45:35 -0500

    Unfinished Lab 1.4

---
+++ month.cpp
@@ -0,0 +1,12 @@
+/*
+Author: Deziah Gayle
+Course: CSCI-135
+Instructor: Mike Z.; Genedy M
+Assignment: Lab1D
+
+Returns the number of days in a given month
+*/
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
