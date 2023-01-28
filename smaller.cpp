e19682e85d3650d7f3042d2875b0cb36a1b1cf2f
Dezzz <dezzz@Dezzzs-MacBook-Air.local>
2023-01-28 11:46:30 -0500

    Lab 1.1

--- smaller.cpp
+++ smaller.cpp
@@ -1,8 +1,32 @@
-//
-//  smaller.cpp
-//  
-//
-//  Created by Dezzz on 1/28/23.
-//
+/*
+Author: Deziah Gayle
+Course: CSCI-135
+Instructor: Mike Z.; Genedy M
+Assignment: Lab1A
+
+Finding the smaller of two integers
+*/
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Enter the first number: " << endl;
+        int firstNum = 0;
+    cin >> firstNum;
+
+    cout << "Enter the second number: " << endl;
+        int secondNum = 0;
+    cin >> secondNum;
+    
+        if (firstNum > secondNum) {
+            cout << "The smaller of the two is " << secondNum << endl;
+        } else {
+            cout << "The smaller of the two is " << firstNum << endl;
+        }
+
+        return 0;
+}
+
