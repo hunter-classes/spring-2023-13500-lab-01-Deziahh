a24b1c493e9aef08ead3f0c0852e56ad716d1e72
Dezzz <dezzz@Dezzzs-MacBook-Air.local>
2023-02-02 21:59:57 -0500

    Lab 1.4

--- month.cpp
+++ month.cpp
@@ -8,5 +8,29 @@ Returns the number of days in a given month
 */
 
 #include <stdio.h>
-#include <iostream>
 using namespace std;
+#include <iostream>
+
+int main() {
+  int year, month;
+  
+  std::cout << "Enter year: ";
+  std::cin >> year;
+  
+  std::cout << "Enter month (1-12): ";
+  std::cin >> month;
+  
+  if (month == 2) {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+      std::cout << "29 days" << std::endl;
+    } else {
+      std::cout << "28 days" << std::endl;
+    }
+  } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+    std::cout << "30 days" << std::endl;
+  } else {
+    std::cout << "31 days" << std::endl;
+  }
+  
+  return 0;
+}
